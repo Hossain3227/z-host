3add4baf2d3b813567801ccd8b31463707ece2cd
Hossain3227 <hasan32.hk27@gmail.com>
2025-08-11 00:01:09 +0600

    sidebar added

--- client/src/layouts/DashboardLayout.jsx
+++ client/src/layouts/DashboardLayout.jsx
@@ -1,15 +1,19 @@
 import { Outlet } from "react-router-dom";
+import Sidebar from "../components/Dashboard/Sidebar/Sidebar";
 
 
 const DashboardLayout = () => {
     return (
         <div>
-            <div>
-                {/* sidebar */}
+            <div className="relative min-h-screen md:flex">
+                <Sidebar></Sidebar>
             </div>
-            
-            <div>
+
+            <div className="flex-1 md:ml-64">
+                <div className="p-5">
                 <Outlet></Outlet>
+                </div>
+                
             </div>
         </div>
     );
