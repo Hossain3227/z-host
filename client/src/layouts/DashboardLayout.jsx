6ab5f983696682074a87d492e3a9afd30a812002
Hossain3227 <hasan32.hk27@gmail.com>
2025-08-18 11:59:17 +0600

    some bugs fixed

--- client/src/layouts/DashboardLayout.jsx
+++ client/src/layouts/DashboardLayout.jsx
@@ -4,10 +4,10 @@ import Sidebar from "../components/Dashboard/Sidebar/Sidebar";
 
 const DashboardLayout = () => {
     return (
-        <div>
+        <>
             <div className="relative min-h-screen md:flex">
                 <Sidebar></Sidebar>
-            </div>
+            
 
             <div className="flex-1 md:ml-64">
                 <div className="p-5">
@@ -15,7 +15,8 @@ const DashboardLayout = () => {
                 </div>
                 
             </div>
-        </div>
+            </div>
+        </>
     );
 };
 
--- client/src/pages/SignUp/SignUp.jsx
+++ client/src/pages/SignUp/SignUp.jsx
@@ -4,6 +4,7 @@ import useAuth from '../../hooks/useAuth'
 import axios from 'axios'
 import toast from 'react-hot-toast'
 import { TbFidgetSpinner } from 'react-icons/tb'
+import { imageUpload } from '../../api/utils'
 
 const SignUp = () => {
   const navigate = useNavigate()
@@ -28,20 +29,16 @@ const SignUp = () => {
     try {
       setLoading(true)
       // 1. Upload image and get image url
-      const { data } = await axios.post(
-        `https://api.imgbb.com/1/upload?key=${
-          import.meta.env.VITE_IMGBB_API_KEY
-        }`,
-        formData
-      )
-      console.log(data.data.display_url)
+      const image_url = await imageUpload(image)
+      console.log(image_url);
+      
 
       //2. User Registration
       const result = await createUser(email, password)
       console.log(result)
 
       // 3. Save username and photo in firebase
-      await updateUserProfile(name, data.data.display_url)
+      await updateUserProfile(name, image_url)
       navigate('/')
       toast.success('Signup Successful')
     } catch (err) {
